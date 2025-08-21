59277c1c72e122854a1146ebae501a3108283d07
Ankit Mehta <183965198+9112000@users.noreply.github.com>
2025-08-21 09:04:41 +0530

    Initial commit

--- src/script.js
+++ src/script.js
@@ -479,13 +479,13 @@
             }
         });
         
-       if ("serviceWorker" in navigator) {
-           window.addEventListener("load", () => {
-               navigator.serviceWorker
-               .register("/sw.js")
-               .then(reg => console.log("Service worker registered", reg))
-               .catch(err => console.error("Service worker registration failed ", err));
-               
-           });
-           
-       }
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", () => {
+                navigator.serviceWorker
+                .register("/sw.js")
+                .then(reg => console.log("Service worker registered", reg))
+                .catch(err => console.error("Service worker registration failed ", err));
+                
+            });
+            
+        }

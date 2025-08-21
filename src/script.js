c61561bed3ab7e491da5188a3ada9e031b3cfe56
Ankit Mehta <183965198+9112000@users.noreply.github.com>
2025-08-21 09:19:19 +0530

    Initial commit

--- src/script.js
+++ src/script.js
@@ -482,7 +482,7 @@
         if ("serviceWorker" in navigator) {
             window.addEventListener("load", () => {
                 navigator.serviceWorker
-                .register("/sw.js")
+                .register("./sw.js")
                 .then(reg => console.log("Service worker registered", reg))
                 .catch(err => console.error("Service worker registration failed ", err));
                 
